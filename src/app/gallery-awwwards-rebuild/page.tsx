a550c3fed388aa7f189d9acdcaf563252ac115aa
tobisamcode <samueloluwatobiloba48@gmail.com>
2026-06-23 21:14:14 +0100

    gallery-awwwards-rebuild.png

---
+++ src/app/gallery-awwwards-rebuild/page.tsx
@@ -0,0 +1,9 @@
+import GalleryAwwwardsRebuildPage from "@/features/gallery-awwwards-rebuild/components/gallery-awwwards-rebuild-page";
+
+export const metadata = {
+  title: "Gallery Awwwards Rebuild | Codegrid",
+};
+
+export default function Page() {
+  return <GalleryAwwwardsRebuildPage />;
+}
