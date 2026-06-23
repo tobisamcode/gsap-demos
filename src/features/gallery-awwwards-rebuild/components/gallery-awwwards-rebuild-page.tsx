89347f57de6da98680e4ee87aa6cf2fa99dce81f
tobisamcode <samueloluwatobiloba48@gmail.com>
2026-06-23 21:14:05 +0100

    gallery-awwwards-rebuild.png

---
+++ src/features/gallery-awwwards-rebuild/components/gallery-awwwards-rebuild-page.tsx
@@ -0,0 +1,6 @@
+import GalleryAwwwardsRebuild from "@/features/gallery-awwwards-rebuild/components/gallery-awwwards-rebuild";
+import "@/features/gallery-awwwards-rebuild/styles/gallery-awwwards-rebuild.scss";
+
+export default function GalleryAwwwardsRebuildPage() {
+  return <GalleryAwwwardsRebuild />;
+}
